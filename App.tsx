eea5fb9243e5b15b57a8621dadab870370c7e91f
Brodo8042 <67292707+Brodo8042@users.noreply.github.com>
2021-12-21 18:04:44 +0300

    add: navigation

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Home from './src/screens/Home';
 import { Provider } from 'react-redux';
 import { store } from './src/store/Store';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    </NavigationContainer>
   );
 };
 
